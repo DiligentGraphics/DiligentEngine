60c79033b4cd76fb82c6d6213e76559832bbd55d
Egor Yusov <egor.yusov@gmail.com>
2019-11-24 22:26:22 -0800

    Updated modules (clang-formatted source); enabled formatting validation

---
+++ Utilities/FormatValidation/validate_format_win.bat
@@ -0,0 +1,6 @@
+python ../../DiligentCore/Utilities/FormatValidation/clang-format-validate.py --color never --clang-format-executable ../../DiligentCore/Utilities/FormatValidation/clang-format_10.0.0.exe ^
+-r ../../DiligentFX/Components ../../DiligentFX/GLTF_PBR_Renderer ../../DiligentFX/PostProcess ../../DiligentFX/Utilities ^
+../../DiligentTools/AssetLoader ../../DiligentTools/Imgui ../../DiligentTools/NativeApp/include  ../../DiligentTools/NativeApp/src ^
+../../DiligentTools/TextureLoader ^
+ --exclude ../../DiligentTools/Imgui/interface/ImGuiImplMacOS.h --exclude ../../DiligentTools/Imgui/interface/ImGuiImplIOS.h ^
+ --exclude ../../DiligentTools/NativeApp/src/UWP --exclude ../../DiligentTools/NativeApp/include/UWP
