6c1033cb44b4d66c1ade8957e8a77786e81e888c
assiduous <assiduous@diligentgraphics.com>
2020-10-11 19:15:56 -0700

    Updated test_all.bat file

--- Tests/test_all.bat
+++ Tests/test_all.bat
@@ -8,18 +8,28 @@ for %%x in (%*) do set /A num_args+=1
 if "%num_args%" LSS "3" (
     echo Command line format:
     echo.
-    echo   test_all.bat config build_path golden_images_path
+    echo   test_all.bat build_path config golden_images_path
     echo.
-    echo config             - configuration (Debug, Release, etc.^)
     echo build_path         - path to the root of the build tree
+    echo config             - configuration (Debug, Release, etc.^)
     echo golden_images_path - path to the golden images root folder
     echo.
     echo Example:
-    echo   test_all.bat Debug c:\Projects\DiligentEngine\build\Win64 c:\Projects\DiligentEngine\GoldenImages
+    echo   test_all.bat c:\Projects\DiligentEngine\build\Win64 Debug c:\Projects\DiligentEngine\GoldenImages
     echo.
     echo To capture golden images, replace 'capture' with 'compare' in the script below
     EXIT /B -1
 )
+
+set build_folder=%~1
+shift
+
+set config=%1
+shift
+
+set golden_images_root_dir=%~1
+shift
+
 
 set D3D11_ERROR=0
 set D3D12_ERROR=0
@@ -31,7 +41,7 @@ set CURR_DIR=%cd%
 
 cd ..\DiligentCore\Tests\DiligentCoreAPITest\assets
 
-set API_TEST_EXE_PATH="%2\DiligentCore\Tests\DiligentCoreAPITest\%1\DiligentCoreAPITest.exe"
+set API_TEST_EXE_PATH="%build_folder%\DiligentCore\Tests\DiligentCoreAPITest\%config%\DiligentCoreAPITest.exe"
 
 (%API_TEST_EXE_PATH% --mode=d3d11) || set /a D3D11_ERROR=%D3D11_ERROR%+1
 (%API_TEST_EXE_PATH% --mode=d3d11_sw) || set /a D3D11_ERROR=%D3D11_ERROR%+10
@@ -48,10 +58,9 @@ set API_TEST_EXE_PATH="%2\DiligentCore\Tests\DiligentCoreAPITest\%1\DiligentCore
 
 
 cd "%CURR_DIR%\..\DiligentSamples\Tests\GoldenImages"
-set golden_images_root_dir=%3
 set golden_img_width=1024
 set golden_img_height=768
-(call ProcessGoldenImages.bat %2 Debug compare "" d3d11 d3d12 gl vk) || set APP_ERROR=!ERRORLEVEL!
+(call ProcessGoldenImages.bat %build_folder% %config% compare "" d3d11 d3d12 gl vk) || set APP_ERROR=!ERRORLEVEL!
 
 cd "%CURR_DIR%"
 
