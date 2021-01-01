09a3e1f67510042f0495f6125a062de860d9c66b
assiduous <assiduous@diligentgraphics.com>
2021-01-01 12:54:41 -0800

    Updated copyright notice

--- unityplugin/GhostCubeScene/src/GhostCubeSceneResTrsnHelper.h
+++ unityplugin/GhostCubeScene/src/GhostCubeSceneResTrsnHelper.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2019-2020 Diligent Graphics LLC
+ *  Copyright 2019-2021 Diligent Graphics LLC
  *  Copyright 2015-2019 Egor Yusov
  *  
  *  Licensed under the Apache License, Version 2.0 (the "License");
